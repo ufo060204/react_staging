5607b55052184000d81936b36a3a92656b54e8e6
ufo060204 <ufo060204@gmail.com>
2024-07-16 00:16:42 +0800

    8th commit

--- 63_src_路由表/App.jsx
+++ 63_src_路由表/App.jsx
@@ -8,20 +8,20 @@ export default function App() {
   return (
     <div className='container'>
       <div className='row'>
-        <div className='col-offset-2 col-8'>
+        <div className='col-xs-offset-2 col-xs-8'>
           <div className='page-header'>
             <h2>React Router Demo</h2>
           </div>
         </div>
       </div>
       <div className='row'>
-        <div className='col-offset-2 col-2'>
+        <div className='col-xs-offset-2 col-xs-2'>
           <div className='list-group'>
             <NavLink className='list-group-item' to="/about">About</NavLink>
             <NavLink className='list-group-item' to="/home">Home</NavLink>
           </div>
         </div>
-        <div className='col-6'>
+        <div className='col-xs-6 col-xs-offset-2'>
           <div className='panel'>
             <div className='panel-body'>
               {/* 註冊路由 */}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { NavLink, Routes, Route, Outlet } from 'react-router-dom'
+// import MyNavLink from '../../components/MyNavLink'
+import News from './News'
+import Message from './Message'
 
 export default function Home() {
   return (
     <div>
       <h3>我是 home 內容</h3>
+      <div>
+        <ul className='nav nav-tabs'>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to="/home/news">News</NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to="/home/message">Message</NavLink>
+          </li>
+        </ul>
+        <Outlet />
+        {/* 註冊路由 */}
+        <Routes>
+          <Route path='/home/news' element={<News />}/>
+          <Route path='/home/message' element={<Message />}/>
+        </Routes>
+      </div>
     </div>
   )
 }
